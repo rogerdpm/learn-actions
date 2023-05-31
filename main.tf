a556fe923a5ef8d7ad5415feaf9a86355638e4d8
rogerdpm <rogerdpm@gmail.com>
2023-05-31 17:42:44 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "remote" {
+    organization = "wecolla8"
+
+    workspaces {
+      name = "taexam"
+    }
+  }
+}
+
+    resource "null_resource" "example" {
+      triggers = {
+        value = "A example resource that does nothing!"
+      }
+    }
